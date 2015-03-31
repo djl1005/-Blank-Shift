fef3c7ae389a978cc5298e2e83b1a506bab4ce25
Daniel Law <djl1005@rit.edu>
2015-03-31 01:47:37 -0400

    Click to select tile now added

--- js/game.js
+++ js/game.js
@@ -6,6 +6,12 @@ window.onload = function(){
 
 	app.game = new Phaser.Game(300,300,Phaser.CANVAS,"",{preload:onPreload, create:onCreate, update:onUpdate});
 	
+	app.startX;
+	app.startY;
+	app.dragging;
+	app.movingRow;
+	app.movingCol;
+	
 	var tileSize = 50;				// tile size, in pixels
 	var fieldSize = 6;     			// number of tiles per row/column
 	var tileTypes = 6;				// different kind of tiles allowed
@@ -38,20 +44,41 @@ window.onload = function(){
 				tileArray[i][j]=theTile;
 				tileGroup.add(theTile);	
 			}
+			app.game.input.onDown.add(pickTile, this);
 		}	
+	}
+	
+	function pickTile(){
+		// save input coordinates
+		app.startX = app.game.input.worldX;
+		app.startY = app.game.input.worldY;
+		// retrieve picked row and column 
+		
+		console.log(app.game.world.height - app.startY);
+		
+		app.movingRow = Math.floor(( app.game.world.height - app.startY )/tileSize);
+		app.movingCol = Math.floor( ( app.game.world.width - app.startX ) /tileSize);
+		// move the tile to the upper group, so it will surely be at top of the stage
+		movingTileGroup.add(tileArray[app.movingRow][app.movingCol]);
+		// zoom the tile
+		tileArray[app.movingRow][app.movingCol].width=tileSize*pickedZoom;
+		tileArray[app.movingRow][app.movingCol].height=tileSize*pickedZoom;
+		// now dragging is allowed
+		app.dragging = true ;
+		// update listeners
+		app.game.input.onDown.remove(pickTile, this);
+		//game.input.onUp.add(releaseTile, this);
 	}
 	
 	function onUpdate() {
 		// if we are dragging a tile
-		if(false){
+		if(app.dragging){
 			// check x and y distance from starting to current input location
-			distX = game.input.worldX-startX;
-							distY = game.input.worldY-startY;
-							// move the tile
-							tileArray[movingRow][movingCol].x=movingCol*tileSize+tileSize/2+distX;
-							tileArray[movingRow][movingCol].y=movingRow*tileSize+tileSize/2+distY;
+			// move the tile
+			tileArray[app.movingRow][app.movingCol].x= app.game.input.worldX;
+			tileArray[app.movingRow][app.movingCol].y= app.game.input.worldY;
 		}
-		}
+	}
 	
 	
 };
