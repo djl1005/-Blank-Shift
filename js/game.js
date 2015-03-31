b1d6080046785946a148c675b381652def6085a7
Daniel Law <djl1005@rit.edu>
2015-03-30 22:35:44 -0400

    Some felids

--- js/game.js
+++ js/game.js
@@ -2,4 +2,13 @@
 
 var app = app || {};
 
- app.game = new Phaser.Game(800, 600, Phaser.AUTO)
+app.game = new Phaser.Game(800, 600, Phaser.AUTO);
+ 
+var tileSize = 50;				// tile size, in pixels
+var fieldSize = 6;     			// number of tiles per row/column
+var tileTypes = 6;				// different kind of tiles allowed
+var pickedZoom = 1.1;              // zoom ratio to highlight picked tile
+
+var tileArray = [];				// array with all game tiles
+	var tileGroup; 				// group containing all tiles
+	var movingTileGroup;               // group containing the moving tile
